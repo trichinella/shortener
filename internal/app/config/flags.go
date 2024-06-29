11a4ae9bc57c935171548e7da4046c168c36809f
trihin <a.trichinella@gmail.com>
2024-06-29 10:44:37 +0300

    Инкремент 9
    Исправления по код-ревью

--- internal/app/config/flags.go
+++ internal/app/config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Options struct {
 	ServerHost      string
@@ -13,6 +16,5 @@ var BaseOptions = Options{}
 func init() {
 	flag.StringVar(&BaseOptions.ServerHost, "a", "localhost:8080", "Server host")
 	flag.StringVar(&BaseOptions.DisplayLink, "b", "http://localhost:8080", "Link displays for user")
-	//flag.StringVar(&BaseOptions.FileStoragePath, "f", "/tmp/short-url-db.json", "File path for storage")
-	flag.StringVar(&BaseOptions.FileStoragePath, "f", "/home/alexey/dev/shortener/short-url-db.json", "File path for storage")
+	flag.StringVar(&BaseOptions.FileStoragePath, "f", os.TempDir()+"/short-url-db-test.json", "File path for storage")
 }
